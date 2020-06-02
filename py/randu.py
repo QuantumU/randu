e73e72ad5cc63334e33abea395f07828df619558
fabian.luehrig <fabian.luehrig@stud.uni-goettingen.de>
2020-06-02 14:45:37 +0200

    changed plot layout

--- py/randu.py
+++ py/randu.py
@@ -2,13 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
+fig = plt.figure(figsize=(10,7.5))
 ax = fig.add_axes([0,0,1,1], projection='3d') # use 3d plotting
 
 #----------------------------layout-------------------------------------
-fig = plt.figure(figsize=(10,7.5))
 plt.style.use('ggplot')
 fig.set_facecolor('w')
-ax.set_facecolor('w')
 plt.subplots_adjust(left=0.15, right=0.9, top=0.85, bottom=0.15)
 plt.xticks(fontname='Ubuntu Mono', fontsize=12)
 plt.yticks(fontname='Ubuntu Mono', fontsize=12)
