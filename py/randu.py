7ccf72534ef1cff280462984f1c747647ef7c406
fabian.luehrig <fabian.luehrig@stud.uni-goettingen.de>
2020-06-02 14:30:23 +0200

    added c source, python script and example data

---
+++ py/randu.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+ax = fig.add_axes([0,0,1,1], projection='3d') # use 3d plotting
+
+#----------------------------layout-------------------------------------
+fig = plt.figure(figsize=(10,7.5))
+plt.style.use('ggplot')
+fig.set_facecolor('w')
+ax.set_facecolor('w')
+plt.subplots_adjust(left=0.15, right=0.9, top=0.85, bottom=0.15)
+plt.xticks(fontname='Ubuntu Mono', fontsize=12)
+plt.yticks(fontname='Ubuntu Mono', fontsize=12)
+#-----------------------------------------------------------------------
+
+x, y, z = np.loadtxt(".dat/randu.csv", delimiter='\t', unpack=True) # load data from file
+
+ax.plot(x,y,z,'.')
+plt.show()
