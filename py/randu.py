74693f65f8492f90e9bae91e09d80884ebd2f56d
fabian.luehrig <fabian.luehrig@stud.uni-goettingen.de>
2020-06-02 15:16:43 +0200

    added animation script

--- py/randu.py
+++ py/randu.py
@@ -16,4 +16,15 @@ plt.yticks(fontname='Ubuntu Mono', fontsize=12)
 x, y, z = np.loadtxt(".dat/randu.csv", delimiter='\t', unpack=True) # load data from file
 
 ax.plot(x,y,z,'.')
+ax.view_init(45,-45) # set initial view
+plt.draw()
+plt.pause(2) # wait 2 secs, then start rotating
+
+for angle in range(45, 110): # rotate the view
+    ax.view_init(angle,-45)
+    plt.draw()
+    plt.pause(.01)
+
+plt.pause(2)
+ax.view_init(45,-45) # set initial view
 plt.show()
